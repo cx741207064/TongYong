e3c0f2e75be782240937ca739df20223071a15a4
cx <741207064@qq.com>
2020-02-27 11:30:10 +0800

    公式调整

--- Code/ProduceSource/JlueTaxSystemTongYongBS/Controllers/jgcxController.cs
+++ Code/ProduceSource/JlueTaxSystemTongYongBS/Controllers/jgcxController.cs
@@ -38,47 +38,95 @@ namespace JlueTaxSystemHeBeiBS.Controllers
             string zsxmDm = in_jo["zsxmDm"].ToString();
             if (!string.IsNullOrEmpty(zsxmDm))
             {
-                if (zsxmDm == "10101") { sbxh = "001"; }else{ sbxh = ""; }
+                if (zsxmDm == "10101") { sbxh = "001"; } else { sbxh = ""; }
                 listqc = listqc.Where(a => a.zsxmDm == zsxmDm).ToList();
+                foreach (GDTXUserYSBQC qc in listqc)
+                {
+
+                    JToken jt = set.getUserYSBQCReportData(qc.Id, qc.sbzlDm, sbxh);
+                    string sbse = set.getSBSE(qc.sbzlDm, jt);
+                    JObject jo = new JObject();
+                    jo.Add("sbxh", sbxh);
+                    jo.Add("djxh", "");
+                    jo.Add("nsrsbh", "");
+                    jo.Add("zsxmDm", qc.zsxmDm);
+                    jo.Add("sbzlDm", qc.sbzlDm);
+                    jo.Add("sbzlMc", qc.ZSXM);
+                    jo.Add("sbse", sbse);
+                    jo.Add("skssqq", qc.SKSSQQ);
+                    jo.Add("skssqz", qc.SKSSQZ);
+                    jo.Add("sbztDm", "0000");
+                    jo.Add("sbztms", "申报成功");
+                    jo.Add("sbrq", qc.HappenDate);
+                    jo.Add("lrsj", qc.HappenDate);
+                    jo.Add("sblxDm", "11");
+                    jo.Add("pzxh", "");
+                    jo.Add("qqwjm", "");
+                    jo.Add("sbny", "");
+                    jo.Add("scpzxh", "");
+                    jo.Add("qdid", "ServyouClient");
+                    jo.Add("yzpzzlDm", "");
+                    jo.Add("czDmList", new JArray { "04" });
+                    jo.Add("sl", "");
+                    jo.Add("sbuuid", "");
+                    jo.Add("gzlxDm", "1");
+                    jo.Add("zsZsxmDm", "");
+                    jo.Add("zsSbzlDm", "");
+                    jo.Add("ybjcbz", "");
+                    jo.Add("gdsBz", "1");
+                    jo.Add("sbzlDlDm", "");
+                    jo.Add("wbtbsbbz", "");
+                    jo.Add("yhsBz", "");
+                    value.Add(jo);
+                }
+               
             }
-            foreach (GDTXUserYSBQC qc in listqc)
+            else
             {
-                
-                JToken jt = set.getUserYSBQCReportData(qc.Id, qc.sbzlDm, out sbxh);
-                string sbse = set.getSBSE(qc.sbzlDm, jt);
-                JObject jo = new JObject();
-                jo.Add("sbxh", sbxh);
-                jo.Add("djxh", "");
-                jo.Add("nsrsbh", "");
-                jo.Add("zsxmDm", qc.zsxmDm);
-                jo.Add("sbzlDm", qc.sbzlDm);
-                jo.Add("sbzlMc", qc.ZSXM);
-                jo.Add("sbse", sbse);
-                jo.Add("skssqq", qc.SKSSQQ);
-                jo.Add("skssqz", qc.SKSSQZ);
-                jo.Add("sbztDm", "0000");
-                jo.Add("sbztms", "申报成功");
-                jo.Add("sbrq", qc.HappenDate);
-                jo.Add("lrsj", qc.HappenDate);
-                jo.Add("sblxDm", "11");
-                jo.Add("pzxh", "");
-                jo.Add("qqwjm", "");
-                jo.Add("sbny", "");
-                jo.Add("scpzxh", "");
-                jo.Add("qdid", "ServyouClient");
-                jo.Add("yzpzzlDm", "");
-                jo.Add("czDmList", new JArray { "04" });
-                jo.Add("sl", "");
-                jo.Add("sbuuid", "");
-                jo.Add("gzlxDm", "1");
-                jo.Add("zsZsxmDm", "");
-                jo.Add("zsSbzlDm", "");
-                jo.Add("ybjcbz", "");
-                jo.Add("gdsBz", "1");
-                jo.Add("sbzlDlDm", "");
-                jo.Add("wbtbsbbz", "");
-                jo.Add("yhsBz", "");
-                value.Add(jo);
+                JToken jt;
+                //listqc = listqc.ToList();
+                foreach (GDTXUserYSBQC qc in listqc)
+                {
+                    if (qc.sbzlDm == "10101")
+                    { sbxh = "001";   jt = set.getUserYSBQCReportData(qc.Id, qc.sbzlDm, sbxh);}
+                    else { sbxh = "";  jt = set.getUserYSBQCReportData(qc.Id, qc.sbzlDm, out sbxh); }
+                   
+                    string sbse = set.getSBSE(qc.sbzlDm, jt);
+                    JObject jo = new JObject();
+                    jo.Add("sbxh", sbxh);
+                    jo.Add("djxh", "");
+                    jo.Add("nsrsbh", "");
+                    jo.Add("zsxmDm", qc.zsxmDm);
+                    jo.Add("sbzlDm", qc.sbzlDm);
+                    jo.Add("sbzlMc", qc.ZSXM);
+                    jo.Add("sbse", sbse);
+                    jo.Add("skssqq", qc.SKSSQQ);
+                    jo.Add("skssqz", qc.SKSSQZ);
+                    jo.Add("sbztDm", "0000");
+                    jo.Add("sbztms", "申报成功");
+                    jo.Add("sbrq", qc.HappenDate);
+                    jo.Add("lrsj", qc.HappenDate);
+                    jo.Add("sblxDm", "11");
+                    jo.Add("pzxh", "");
+                    jo.Add("qqwjm", "");
+                    jo.Add("sbny", "");
+                    jo.Add("scpzxh", "");
+                    jo.Add("qdid", "ServyouClient");
+                    jo.Add("yzpzzlDm", "");
+                    jo.Add("czDmList", new JArray { "04" });
+                    jo.Add("sl", "");
+                    jo.Add("sbuuid", "");
+                    jo.Add("gzlxDm", "1");
+                    jo.Add("zsZsxmDm", "");
+                    jo.Add("zsSbzlDm", "");
+                    jo.Add("ybjcbz", "");
+                    jo.Add("gdsBz", "1");
+                    jo.Add("sbzlDlDm", "");
+                    jo.Add("wbtbsbbz", "");
+                    jo.Add("yhsBz", "");
+                    value.Add(jo);
+                }
+               
             }
             re_json["value"] = value;
             return re_json;
